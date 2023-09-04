d8efdca7fc1106a9d4830bde702020ab72f5bf6c
freddie <mercerfrederick@gmail.com>
2023-09-04 14:41:12 +0100

    Ready for coding

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # STP tutorial problems 
 
-Initial commit 
+# Download and install dependencies in conda env
+```bash
+git clone --branch "main" "https://github.com/Fr3dMer/UoM_stp_tutorials.git" 
+cd UoM_stp_tutorials
+conda create --name UoM_stp_tutorials pip
+conda activate UoM_stp_tutorials
+pip install -r requirements.txt
+```
+
+# Save env
+```bash
+pipreqs --force . 
+```
+
+Plan 
+- [ ] Create logging module
+- [ ] Create arg parser
+- [ ] Tests for Exercise 1 
+- [ ] Exercise 1
+- [ ] Tests for Exercise 2 
+- [ ] Exercise 2
+- [ ] Tests for Exercise 3 
+- [ ] Exercise 3
+- [ ] Tests for Exercise 4 
+- [ ] Exercise 4 
--- main.py
+++ main.py
@@ -4,3 +4,29 @@ About         : Entry point into the program to tie everything together
 Author        : Freddie Mercer
     
 *************************************************************************************************"""
+
+
+
+# Main entry into programme, scaffold for logic 
+def main():
+    
+    # Initialise logging object
+
+
+    # Process args 
+
+
+    # Construct exercise 1 object and execute associated problems 
+
+
+    # Construct exercise 2 object and execute associated problems 
+
+
+    # Construct exercise 3 object and execute associated problems 
+
+
+    # Construct exercise 4 object and execute associated problems 
+
+
+    # Exit main
+    return True
